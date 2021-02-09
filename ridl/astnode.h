e7a984b934cc08521ae1b9cd37a7fa55adcc89ac
zhiming <zhiwood@hotmail.com>
2021-02-09 14:11:04 +0800

    . astnode.h, ridl.y++: complete the grammar actions

--- ridl/astnode.h
+++ ridl/astnode.h
@@ -10,6 +10,28 @@ if( !pObj.IsEmpty() ) \
     pnode->SetParent( this ); \
 }
 
+enum EnumMyClsid
+{
+    DECL_CLSID( CAttrExp ) = clsid( ClassFactoryStart ) + 40,
+    DECEL_CLSID( CAttrExps ),
+    DECEL_CLSID( CPrimeType ),
+    DECEL_CLSID( CArrayType ),
+    DECEL_CLSID( CMapType ),
+    DECEL_CLSID( CStructRef ),
+    DECEL_CLSID( CFieldDecl ),
+    DECEL_CLSID( CFieldList ),
+    DECEL_CLSID( CStructDecl ),
+    DECEL_CLSID( CFormalArg ),
+    DECEL_CLSID( CArgList ),
+    DECEL_CLSID( CMethodDecl ),
+    DECEL_CLSID( CMethodDecls ),
+    DECEL_CLSID( CInterfaceDecl ),
+    DECEL_CLSID( CInterfRef ),
+    DECEL_CLSID( CInterfRefs ),
+    DECEL_CLSID( CServiceDecl ),
+    DECEL_CLSID( CStatements ),
+};
+
 // declarations
 struct CDeclMap
 {
@@ -43,7 +65,8 @@ extern CDeclMap g_oDeclMap;
 struct CAstNodeBase :
     public CObjBase
 {
-    CAstNodeBase() {} 
+    typedef CObjBase super;
+    CAstNodeBase():super() {} 
     gint32 m_iType;
     ObjPtr m_pParent;
 
@@ -55,14 +78,28 @@ struct CAstNodeBase :
     }
 };
 
+struct CNamedNode :
+    public CAstNodeBase
+{
+    typedef CAstNodeBase super;
+    std::string m_strName;
+
+    inline void SetName(
+        const std::string& strName )
+    { m_strName = strName; }
+
+    inline const std::string& GetName() const
+    { return m_strName; }
+}
+
 typedef CAutoPtr< clsid( Invalid ), CAstNodeBase > NodePtr;
 
-struct CAstTreeNode : CAstNodeBase
+struct CAstListNode : CAstNodeBase
 {
     ObjPtr m_pSibling;
     std::vector< ObjPtr > m_queChilds;
     typedef CAstNodeBase super;
-    CAstTreeNode() : CAstNodeBase()
+    CAstListNode() : CAstNodeBase()
     {}
 
     inline void SetSibling( ObjPtr& pSib )
@@ -119,10 +156,10 @@ struct CAttrExp : public CAstNodeBase
     { return m_pVal; }
 };
 
-struct CAttrExps : public CAstTreeNode
+struct CAttrExps : public CAstListNode
 {
-    typedef CAstTreeNode super;
-    CAttrExps() : CAstTreeNode()
+    typedef CAstListNode super;
+    CAttrExps() : CAstListNode()
     { SetClassId( clsid( CAttrExps ) ); }
 
 };
@@ -180,10 +217,11 @@ struct CMapType : public CArrayType
 struct CStructRef : public CPrimeType
 {
     typedef CPrimeType super;
-    std::string m_strName;
 
     CStructRef() : super()
     { SetClassId( clsid( CStructRef ); }
+
+    std::string m_strName;
 
     inline void SetName(
         const std::string& strName )
@@ -193,45 +231,38 @@ struct CStructRef : public CPrimeType
     { return m_strName; }
 };
 
-struct CFieldDecl : public CAstNodeBase
+struct CFieldDecl : public CNamedNode
 {
-    typedef CAstNodeBase super;
-    std::string m_strName;
+    typedef CNamedNode super;
     ObjPtr m_pType;
 
     CFieldDecl() : super()
     { SetClassId( clsid( CFieldDecl ); }
 
-    inline void SetName(
-        const std::string& strName )
-    { m_strName = strName; }
+    void SetType( ObjPtr& pType )
+    {
+        m_pType = pType;
+        SET_PARENT_OF( pType );
+    }
 
-    inline const std::string& GetName() const
-    { return m_strName; }
+    ObjPtr& GetType()
+    { return m_pType; }
 };
 
-struct CFieldList : public CAstTreeNode
+struct CFieldList : public CAstListNode
 {
-    typedef CAstTreeNode super;
+    typedef CAstListNode super;
     CFieldList() : super()
     { SetClassId( clsid( CFieldList ) ); }
 };
 
-struct CStructDecl : public CAstNodeBase
+struct CStructDecl : public CNamedNode
 {
-    typedef CAstNodeBase super;
-    std::string m_strName;
+    typedef CNamedNode super;
     ObjPtr m_oFieldList;
 
     CStructDecl() : super()
     { SetClassId( clsid( CStructDecl ); }
-
-    inline void SetName(
-        const std::string& strName )
-    { m_strName = strName; }
-
-    inline const std::string& GetName() const
-    { return m_strName; }
 
     void SetFieldList( ObjPtr& oFields )
     {
@@ -243,4 +274,114 @@ struct CStructDecl : public CAstNodeBase
     { return m_oFieldList; }
 };
 
+struct CFormalArg : public CFieldDecl
+{
+    typedef CFieldDecl super;
+    CFormalArg() : super()
+    { SetClassId( clsid( CFormalArg ) ); }
+};
 
+struct CArgList : public CAstListNode
+{
+    typedef CAstListNode super;
+    CArgList() : super()
+    { SetClassId( clsid( CArgList ) ); }
+};
+
+struct CMethodDecl : public CNamedNode
+{
+    ObjPtr m_pInArgs;
+    ObjPtr m_pOutArgs;
+
+    typedef CNamedNode super;
+
+    CMethodDecl() : super()
+    { SetClassId( clsid( CMethodDecl ) ); }
+
+    void SetInArgs( ObjPtr& pInArgs )
+    {
+        m_pInArgs = pInArgs;
+        SET_PARENT_OF( pInArgs );
+    }
+    ObjPtr& GetInArgs()
+    { return m_pInArgs; }
+
+    void SetOutArgs( ObjPtr& pOutArgs )
+    {
+        m_pOutArgs = pOutArgs;
+        SET_PARENT_OF( pOutArgs );
+    }
+    ObjPtr& GetOutArgs()
+    { return m_pOutArgs; }
+};
+
+struct CMethodDecls : public CAstListNode
+{
+    typedef CAstListNode super;
+    CMethodDecls() : super()
+    { SetClassId( clsid( CMethodDecls ) ); }
+};
+
+struct CInterfaceDecl : public CNamedNode
+{
+    typedef CNamedNode super;
+    ObjPtr m_pMdl;
+
+    CInterfaceDecl() : super()
+    { SetClassId( clsid( CInterfaceDecl ); }
+
+    void SetMethodList( ObjPtr& pMethods )
+    {
+        m_pMdl = pMethods;
+        SET_PARENT_OF( pMethods );
+    }
+
+    ObjPtr& GetMethodList()
+    { return m_pMdl; }
+};
+
+struct CInterfRef : public CNamedNode
+{
+    typedef CNamedNode super;
+    ObjPtr m_pAttrList;
+
+    CInterfRef() : super()
+    { SetClassId( clsid( CInterfRef ); }
+
+    void SetAttrList( ObjPtr& pAttrList )
+    {
+        m_pAttrList = pAttrList;
+        SET_PARENT_OF( pAttrList );
+    }
+    ObjPtr& GetAttrList()
+    { return m_pAttrList; }
+};
+
+struct CInterfRefs : public CAstListNode
+{
+    typedef CAstListNode super;
+    CInterfRefs() : super()
+    { SetClassId( clsid( CInterfRefs ); }
+};
+
+struct CServiceDecl : public CInterfRef
+{
+    ObjPtr m_pInterfList;
+    CInterfRef() : super()
+    { SetClassId( clsid( CServiceDecl ); }
+
+    void SetInterfList( ObjPtr& pInterfList )
+    {
+        m_pInterfList = pInterfList;
+        SET_PARENT_OF( pInterfList );
+    }
+    ObjPtr& GetInterfList()
+    { return m_pInterfList; }
+};
+
+struct CStatements : public CAstListNode
+{
+    typedef CAstListNode super;
+    CStatements() : CAstListNode()
+    { SetClassId( clsid( CStatements ) ); }
+};
