f0f3ec09355aa34d176afb3eacf8de7d7a91d16f
zhiming <zhiwood@hotmail.com>
2021-08-11 16:18:29 +0800

    . python/Makefile.am python/build-4.9.py python/gentoml.sh python/pyproject.tom.tmpl: add to the rpcbase.so the directories where to find combase and ipc, so that the python scripts are not required to have LD_LIBRARY_PATH set before running.

--- python/build-4.9.py
+++ python/build-4.9.py
@@ -1,8 +1,12 @@
 import os
 import sipconfig
+import sys
 
 build_file = "rpcf.sbf"
 config=sipconfig.Configuration()
+
+if len( sys.argv ) > 1 :
+    libdir=sys.argv[ 1 ]
 
 #os.system( "rm ./sip4build/*" )
 os.system( " ".join([config.sip_bin, "-c", "./sip4build", "-b", build_file, "rpcf.sip" ] ) )
@@ -12,5 +16,10 @@ makefile.extra_libs = ["combase", "ipc" ]
 makefile.extra_defines = ["DEBUG","_USE_LIBEV" ]
 makefile.extra_library_dirs = ["../../combase/.libs", "../../ipc/.libs" ]
 makefile.extra_include_dirs = [ "../../include","../../ipc", "../../test/stmtest", "/usr/include/dbus-1.0", "/usr/lib/x86_64-linux-gnu/dbus-1.0/include", "/usr/include/jsoncpp" ]
+
+if libdir is not None :
+    rpaths = "-Wl,-rpath=" + libdir + ",-rpath=" + libdir + "/rpcf"
+    makefile.extra_lflags.append( rpaths )
+
 makefile.generate()
 os.system( "make -C ./sip4build" )
