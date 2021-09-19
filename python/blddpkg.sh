d37d6a18d1719a613758aa3b7706309280cc9d27
zhiming <zhiwood@hotmail.com>
2021-09-19 17:52:24 +0800

    . added bldpkg directory for building of debian package and rpm package.

---
+++ python/blddpkg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DESTDIR=$1
+prefix=$2
+pkg=$3
+
+pkgfile=$(basename $pkg)
+pkgname=${pkgfile%%-*}
+
+if ! which dpkg-deb; then
+    exit 0
+fi
+
+if [ ! -e ${DESTDIR}/DEBIAN ]; then mkdir ${DESTDIR}/DEBIAN; fi
+cat << EOF >> ${DESTDIR}/DEBIAN/postinst
+pip3 install ${prefix}/lib/${pkgname}/${pkgfile};
+EOF
+
+cat << EOF >> ${DESTDIR}/DEBIAN/postrm
+pip3 uninstall ${pkgname}
+EOF
+
+chmod 0755 ${DESTDIR}/DEBIAN/postinst
+chmod 0755 ${DESTDIR}/DEBIAN/postrm
