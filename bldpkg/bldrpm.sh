d63c6f680545b3ccb1593fce305456e1ef62fba1
zhiming <zhiwood@hotmail.com>
2021-10-01 16:49:59 +0800

    . fixed some bugs

--- bldpkg/bldrpm.sh
+++ bldpkg/bldrpm.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 SRCTREE=$1
 PACKAGE_NAME=$2
+ARCH=$3
 timestamp=`date -R | awk '{print $1,$3,$2,$4}' | sed 's/,//'`
 echo $timestamp
 sed -i "s:SSSSS:${SRCTREE}:" ${PACKAGE_NAME}.spec
 sed -i "s/XXXXX/${timestamp}/" ${PACKAGE_NAME}.spec
 cp ${PACKAGE_NAME}.spec ${HOME}/rpmbuild/SPECS
-cd ${HOME}/rpmbuild/SPECS && rpmbuild -ba ${PACKAGE_NAME}.spec
+if cd ${HOME}/rpmbuild/SPECS && rpmbuild -ba ${PACKAGE_NAME}.spec; then
+    cp ../SRPMS/*.rpm $SRCTREE/bldpkg/
+    cp ../RPMS/$ARCH/*.rpm $SRCTREE/bldpkg/
+fi
 
