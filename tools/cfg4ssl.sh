4372424870a31129686a1a95650ceb2e50cedeb9
zhiming <woodhead99@gmail.com>
2021-12-24 23:25:31 +0800

    . made some small changes to output config infomation for ssl test

--- tools/cfg4ssl.sh
+++ tools/cfg4ssl.sh
@@ -2,8 +2,10 @@
 keydir=$(pwd)/tools/testcfgs
 pushd ./tools/testcfgs
 sed "s:XXXXX:${keydir}:g" sslcfg.json.in > sslcfg.json
-kill -9 `pidof rpcrouter` || true
-kill -9 `pidof python3` || kill -9 `pidof python` || true
+echo pidof rpcrouter
+pidof rpcrouter
+sudo kill -9 `pidof rpcrouter` || true
+sudo kill -9 `pidof python3` || true
 cat sslcfg.json
 sudo python3 /usr/local/bin/rpcf/rpcfgnui.py sslcfg.json
 popd
