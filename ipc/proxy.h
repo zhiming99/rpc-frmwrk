a1178aac05dd18b9dd51fce6a526d0aafc8de9c8
zhiming <woodhead99@gmail.com>
2022-11-17 20:49:05 +0800

    . proxy.h: changed UserCancelRequest from CInterfaceServer to a virtual method so that, it can be overridden by rpcfs skeltn code.

--- ipc/proxy.h
+++ ipc/proxy.h
@@ -1621,17 +1621,17 @@ class CInterfaceServer :
 
     virtual gint32 InitUserFuncs();
 
-    // a user-initialized cancel request
-    gint32 UserCancelRequest(
+    // user-launched cancel request
+    virtual gint32 UserCancelRequest(
         IEventSink* pInvokeTask,
         guint64 qwIoTaskId );
 
-    // a user-initialized cancel request
+    // user-launched cancel request
     gint32 ForceCancelRequests(
         IEventSink* pInvokeTask,
         ObjPtr& pvecTasks );
 
-    // a user-initialized keep-alive request
+    // user-launched one-way keep-alive request
     gint32 KeepAliveRequest(
         IEventSink* pCallback,
         guint64 qwIoTaskId );
