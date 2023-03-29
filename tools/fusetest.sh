25d64be8e8f934ed08ba3144998e3634e8482c5d
zhiming <woodhead99@gmail.com>
2023-03-29 23:24:08 +0800

    . fusetest.sh: adjusted the waiting time after directory created.

--- tools/fusetest.sh
+++ tools/fusetest.sh
@@ -109,7 +109,7 @@ function singleMkdir()
     fi
     echo mkdir \$targdir
     mkdir \$targdir 2>/dev/null
-    sleep 1
+    sleep 3
     python3 $pydir/testypes/maincli.py ./\$conndir/\$targdir 0
     rmdir ./\$conndir/\$targdir
     popd
