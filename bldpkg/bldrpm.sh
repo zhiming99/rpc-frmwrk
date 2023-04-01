31fb5e21f3a65a7fd8f3ab1e514751972fd8fe42
zhiming <woodhead99@gmail.com>
2023-04-01 17:19:22 +0800

    . bldrpm.sh configure.ac: made changes to convert file seperator in GMSSL_RPMFILES to newline as required by rpcf.spec.

--- bldpkg/bldrpm.sh
+++ bldpkg/bldrpm.sh
@@ -6,6 +6,7 @@ timestamp=`date -R | awk '{print $1,$3,$2,$4}' | sed 's/,//'`
 echo $timestamp
 sed -i "s:SSSSS:${SRCTREE}:" ${PACKAGE_NAME}.spec
 sed -i "s/XXXXX/${timestamp}/" ${PACKAGE_NAME}.spec
+sed -i "s:ZZZZZ:\n:" ${PACKAGE_NAME}.spec
 cp ${PACKAGE_NAME}.spec ${HOME}/rpmbuild/SPECS
 if cd ${HOME}/rpmbuild/SPECS && rpmbuild -ba ${PACKAGE_NAME}.spec; then
     cp ../SRPMS/*.rpm $SRCTREE/bldpkg/
