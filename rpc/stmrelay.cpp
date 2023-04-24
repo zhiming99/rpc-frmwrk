56c990d9fce3624d9e076e47c26576f0e88680cc
zhiming <woodhead99@gmail.com>
2023-04-24 15:03:44 +0800

    . stmrelay.cpp: fixed a typo

--- rpc/stmrelay.cpp
+++ rpc/stmrelay.cpp
@@ -2613,7 +2613,7 @@ gint32 CUnixSockStmProxyRelay::OnDataReceivedRemote(
         if( qwPkts == 0 && qwBytes == 0 )
             break;
 
-        if( ( ( qwPkts > 0 &&
+        if( ( qwPkts > 0 &&
             qwPkts <= ( STM_MAX_PACKETS_REPORT >> 3 ) ) &&
             ( qwBytes > 0 &&
                 qwBytes <= ( STM_MAX_PENDING_WRITE >> 8 ) ) )
