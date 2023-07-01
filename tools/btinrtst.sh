f6c42d44f59e5cd763c4f251a1426bbaf0856ab3
zhiming <woodhead99@gmail.com>
2023-07-02 05:14:46 +0800

    . btinrtst.sh: added debug print

--- tools/btinrtst.sh
+++ tools/btinrtst.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+pkill rpcrouter
 pushd examples/cpp/testypes
+tail -n 18 maincli.cpp
+head -n 22 TestTypesSvcsvr.cpp
 
 echo testing ROS RPC with builtin router
 cat cmdline
 bash cmdline
 make
-release/TestTypessvr -d
+sleep 1
+release/TestTypessvr &
+sleep 2
 release/TestTypescli
 pkill TestTypessvr
 popd
