. proxy.py: fixed a mistake ERROR_NOT_IMPL was assigned the wrong value equal to ERROR_QUEUE_FULL

python/rpcf/proxy.py
@@ -54,7 +54,7 @@ class ErrorCode( IntEnum ) :
     ERROR_CANNOT_COMP = np.int32( np.uint32( 0x8001000b ) )
     ERROR_USER_CANCEL = np.int32( np.uint32( 0x8001000c ) )
     ERROR_PAUSED = np.int32( np.uint32( 0x8001000d ) )
-    ERROR_NOT_IMPL = np.int32( np.uint32( 0x8001000e ) )
+    ERROR_NOT_IMPL = np.int32( np.uint32( 0x80010010 ) )
     ERROR_CANCEL_INSTEAD = np.int32( np.uint32( 0x8001000f ) )
     ERROR_QUEUE_FULL = np.int32( np.uint32( 0x8001000e ) )
 