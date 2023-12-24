b49d3c5bcbed19d5103ca6025d14c04a924408d5
zhiming <woodhead99@gmail.com>
2023-12-24 23:57:42 +0800

    . buildall-deb.sh: fixed two bugs

--- tools/buildall-deb.sh
+++ tools/buildall-deb.sh
@@ -9,9 +9,9 @@ libtool shtool automake autoconf autotools-dev make dbus libdbus-1-3 \
 libdbus-1-dev libjsoncpp-dev libkrb5-3 libkrb5-dev liblz4-1 \
 liblz4-dev lz4 openssl libssl3 libssl-dev libcppunit-1.15-0 libcppunit-dev \
 libfuse3-3 libfuse3-dev krb5-kdc krb5-admin-server krb5-user \
-bash net-tools procps swig openjdk-8-jdk-headless cmake libcommons-cli-java ccache
+bash net-tools procps swig default-jdk-headless cmake libcommons-cli-java ccache
 
-sudo apt-get -y install sip5-tools || apt-get -y install sip-dev python3-sip python3-sip-dev || true
+sudo apt-get -y install sip-tools || apt-get -y install sip-dev python3-sip python3-sip-dev || true
 sudo apt-get -y install libjsoncpp1 || apt-get -y install libjsoncpp25 || apt-get -y install libjsoncpp24
 sudo apt-get -y install git;
 pip3 install wheel numpy || sudo apt-get -y install python3-wheel python3-numpy
