a6225a314bf2eeba65976e72fa453df76bc1e9c6
zhiming <woodhead99@gmail.com>
2024-01-14 15:24:04 +0800

    . configdb.js, msgmatch.js: made some changes for new PR

--- js/combase/msgmatch.js
+++ js/combase/msgmatch.js
@@ -2,7 +2,8 @@ const { messageType } = require("../dbusmsg/constants");
 const { CConfigDb2 } = require("./configdb");
 const { SERI_HEADER_BASE } = require("./defines");
 const CObjBase = require("./objbase");
-const MT = requie( "./enums").EnumMatchType
+const MT = require( "./enums").EnumMatchType
+const EnumPropId = require( "./enums").EnumPropId
 require ("../dbusmsg/message")
 
 const SERI_HEADER = class SERI_HEADER extends SERI_HEADER_BASE
@@ -38,7 +39,8 @@ exports.CMessageMatch = class CMessageMatch extends CObjBase
         this.m_strObjPath = ""
         this.m_strIfName = ""
         this.m_iMatchType = MT.matchInvalid
-        this.m_pCfg = new CConfigDb2()
+        this.m_strMethod = ""
+        this.m_oCfg = new CConfigDb2()
     }
 
     GetType()
@@ -107,6 +109,17 @@ exports.CMessageMatch = class CMessageMatch extends CObjBase
             strMatch += ",interface='" +
                 strIfName + "'"
 
+        strPath = this.GetObjPath()
+        if( strPath.length > 0 )
+            strMatch += ",path='" +
+                strPath + "'"
+
+        strMethod = this.m_oCfg(
+            EnumPropId.propMethodName )
+        if( strMethod !== null )
+            strMatch += ",member='" +
+                strMethod + "'"
+        return strMatch
     }
 
     IsEqual( rhs )
@@ -121,5 +134,14 @@ exports.CMessageMatch = class CMessageMatch extends CObjBase
     }
 
     ToString()
+    {
+        strAll = this.ToDBusRules( messageType.invalid )
+        strAll += ",match="
+        if( this.GetType() == MT.matchClient )
+            strAll += "matchClient"
+        else if( this.GetType() == MT.matchServer )
+            strAll += "matchServer"
+        return strAll
+    }
 
 }
