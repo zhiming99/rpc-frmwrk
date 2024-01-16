0d2afbd7495877827b45d8c4e82bad51cfd1cc12
zhiming <woodhead99@gmail.com>
2024-01-16 12:20:39 +0800

    . factory.js: added the definition of class factory

---
+++ js/combase/factory.js
@@ -0,0 +1,55 @@
+const { CBuffer } = require("./cbuffer")
+const { CConfigDb2 } = require("./configdb")
+const { CMessageMatch } = require("./msgmatch")
+const { CObjBase } = require( "./objbase")
+const Cid = require("./enums").EnumClsid
+
+exports.IClassFactory = class IClassFactory extends CObjBase 
+{
+    constructor()
+    { super() }
+    CreateInstance( iClsid )
+    { return null }
+}
+
+class CClassFactoryBase extends IClassFactory
+{
+    constructor()
+    {
+        super()
+        this.SetClsid( Cid.CClassFactory )
+    }
+
+    CreateInstance( iClsid )
+    {
+        switch( iClsid )
+        {
+        case Cid.CBuffer:
+            return new CBuffer()
+        case Cid.CConfigDb2:
+            return new CConfigDb2()
+        case Cid.CMessageMatch:
+            return new CMessageMatch()
+        case Cid.CStlQwordVector:
+        case Cid.CStlObjVector:
+        default:
+            return null
+        }
+    }
+}
+
+var g_Factories = [ new CClassFactoryBase(), ]
+
+exports.RegisterFactory = function RegisterFactory( oFactory )
+{ g_Factories.push( oFactory ) }
+
+exports.CoCreateInstance = function CoCreateInstance( iClsid )
+{
+    for( i = 0; i < g_Factories.length; i++ )
+    {
+        oObj = g_Factories[i].CreateInstance(iClsid)
+        if( oObj !== null )
+            break
+    }
+    return oObj
+}
