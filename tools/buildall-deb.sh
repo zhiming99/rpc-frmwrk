d3dca9f037a80bbb82da4cf767f288d21b83c4ec
zhiming <woodhead99@gmail.com>
2024-03-11 11:22:56 +0800

    . configure.ac, router/Makefile.am, buildall-deb.sh, buildall-fed.sh: fixed some bugs.

--- tools/buildall-deb.sh
+++ tools/buildall-deb.sh
@@ -8,7 +8,7 @@ else
 fi
 echo installing development tools...
 ${SUDO} apt-get -y install tzdata
-${SUDO} apt-get install krb5-kdc krb5-admin-server krb5-user
+${SUDO} apt-get install krb5-user
 ${SUDO} apt-get install -y gcc g++ python3 python3-dev python3-pip flex bison \
 libtool shtool automake autoconf autotools-dev make dbus libdbus-1-3 \
 libdbus-1-dev libjsoncpp-dev libkrb5-3 libkrb5-dev liblz4-1 \
@@ -19,7 +19,7 @@ bash net-tools procps swig default-jdk-headless cmake libcommons-cli-java ccache
 ${SUDO} apt-get -y install sip-tools || apt-get -y install sip-dev python3-sip python3-sip-dev || true
 ${SUDO} apt-get -y install libjsoncpp1 || apt-get -y install libjsoncpp25 || apt-get -y install libjsoncpp24
 ${SUDO} apt-get -y install git devscripts debhelper expect screen vim npm webpack
-pip3 install wheel numpy || ${SUDO} apt-get -y install python3-wheel python3-numpy
+${SUDO} apt-get -y install python3-wheel python3-numpy || pip3 install wheel numpy
 npm install assert browserify buffer exports long lz4 process put safe-buffer stream xxhash
 
 bash ./makerpcf.sh
