85f2b65ab74d9f3bd84ab11ed950ab5cbc5b4df3
zhiming <woodhead99@gmail.com>
2024-09-05 22:47:58 +0800

    . blkalloc.h: added declaration of block allocator, not complete yet.

--- monitor/registry/blkalloc.h
+++ monitor/registry/blkalloc.h
@@ -22,42 +22,131 @@
  *
  * =====================================================================================
  */
-#include "defines.h"
 #include <arpa/inet.h>
+#include <stdint.h>
 // byte offset
 // 0                             64
 // -------------------------------
-// bytes_in_blk|block_idx_grp| group_idx
+// bytes_in_blk|block_idx_grp|group_idx
 
-#define find_shift( _x )                    ffs( _x )
+// 
+// ---layout---
+// superblock 512b
+// group bitmap 512b( 4096 groups )
+// group0 starts
+//     group0 block bitmap 8kb( 64k blocks )
+//     group0 data blocks
+//         root directory
+// group1 starts
+//     group1 block bitmap 8kb( 64k blocks )
+//     group1 data blocks
+#define find_shift( _x )                    ( ffs( _x ) - 1 )
 
 #define SUPER_BLOCK_SIZE                    512
 #define BLOCK_SHIFT                         9
 #define BLOCK_SIZE                          ( 1 << BLOCK_SHIFT )
 #define BLOCKS_PER_GROUP                    ( 64 * 1024 )
 #define BLKBMP_BLKNUM                       ( BLOCKS_PER_GROUP / ( BLOCK_SIZE * 8 ) )
+#define BLOCK_IDX_MASK                      ( BLOCKS_PER_GROUP - 1 )
 
-#define GROUP_SHIFT                         ( find_shift( PAGE_SIZE ) - 1 )
+#define GROUP_SHIFT                         ( find_shift( PAGE_SIZE ) )
 #define BLKGRP_NUMBER                       ( 1 << GROUP_SHIFT )
 #define GRPBMP_BLKNUM                       ( BLKGRP_NUMBER / ( BLOCK_SIZE * 8 ) )
 #define GRPBMP_SIZE                         ( BLKGRP_NUMBER / 8 )
 #define GRPBMP_START                        ( SUPER_BLOCK_SIZE )
-#define BLKBMP_START                        ( GRPBMP_START + GRPBMP_SIZE )
-#define DATABLK_START                       ( BLKBMP_START + BLKBMP_BLKNUM * BLOCK_SIZE )
 #define GROUP_SIZE                          ( BLOCKS_PER_GROUP * BLOCK_SIZE )
+#define GROUP_IDX_MASK                      ( BLKGRP_NUMBER - 1 )
 
 // group index in a block index
-#define GROUP_IDX_SHIFT                     ( 1 << ( find_shift( GROUP_SIZE ) - 1 ) )
+#define GROUP_IDX_SHIFT                     ( 1 << ( find_shift( GROUP_SIZE ) ) )
 
-#define GROUP_OFFSET( _blk_idx )            ( DATABLK_START + ( _grp_idx )  * GROUP_SIZE )
-#define GROUP_INDEX( _blk_idx )           ( ( _byte_addr >> ( BLOCK_SHIFT )
-#define BLOCK_INDEX( _blk_idx )
-#define BLOCK_OFFSET( _blk_idx ) 
+#define LA_ADDR( _byte_addr )               ( _byte_addr - SUPER_BLOCK_SIZE - GRPBMP_SIZE )
+#define ABS_ADDR( _la_addr )                ( _la_addr + SUPER_BLOCK_SIZE + GRPBMP_SIZE )
 
-template< int BLKSIZE >
-struct CSuperBlock
+// get a tripple( group_idx, blk_idx, offset ) from absolute address
+#define BYTEADDR_TO_OFF_IN_BLK( _byte_addr ) \
+    ( ( LA_ADDR( _byte_addr ) ) & ( BLOCK_SIZE - 1 ) )
+
+#define BYTEADDR_TO_BLKIDX( _byte_addr ) \
+    ( ( ( LA_ADDR( _byte_addr ) ) >> BLOCK_SHIFT  ) & BLOCK_IDX_MASK )
+
+#define BYTEADDR_TO_GRPIDX( _byte_addr ) \
+    ( ( ( LA_ADDR( _byte_addr ) ) >> ( BLOCK_SHIFT + GROUP_SHIFT ) ) & GROUP_IDX_MASK );
+
+#define BYTEADDR_TO_CHS( _byte_addr, _grpidx, _blkidx, _off_in_blk )  \
+({ \
+    _off_in_blk = BYTEADDR_TO_OFF_IN_BLK( _byte_addr );  \
+    _blkidx = BYTEADDR_TO_BLKIDX( _byte_addr ); \
+    _grpidx = BYTEADDR_TO_GRPIDX( _byte_addr ); \
+});
+
+// block address is a combination of block idx and
+// group idx. It is used by inode.
+#define BLKADDR_TO_BLKIDX( _blk_addr ) \
+    ( ( ( _blk_addr ) >>   ) & ( BLOCKS_PER_GROUP - 1 ) )
+
+// get the group idx from the block addr
+#define GROUP_INDEX( _blk_addr ) \
+    ( ( _blk_addr >> BLOCK_SHIFT ) & GROUP_IDX_MASK ) 
+
+// get the logical address of a group from a with a
+// block index from inode
+#define GROUP_START( _blk_addr ) \
+    ( GROUP_INDEX( _blk_addr )  * GROUP_SIZE )
+
+// get the logical address of a group's data section
+// from a with a block index from inode
+#define GROUP_DATA_START( _blk_addr ) \
+    ( GROUP_INDEX( _blk_addr )  * GROUP_SIZE + \
+    BLKBMP_BLKNUM * BLOCK_SIZE )
+
+// get the block idx from the block addr
+#define BLOCK_INDEX( _blk_addr ) \
+    ( ( _blk_addr ) & BLOCK_IDX_MASK )
+
+// get the logical address of a block
+#define BLOCK_LA( _blk_addr ) \
+    ( GROUP_DATA_START( _blk_addr ) + \
+    ( BLOCK_IDX( _blk_addr ) * BLOCK_SIZE ) )
+
+#define BLOCK_ABS( _blk_addr ) \
+    ABS_ADDR( BLOCK_LA( _blk_addr ) )
+
+template< guint32 BLKSIZE >
+struct CBlockAllocator
 {
-    guint32 m_dwMagic = 
+    stdrmutex   m_oLock;
+    guint32     m_dwMagic = 
         htonl( *( guint32* )"regi" );
-    guint32 m_dwVersion = 0x01;
+    guint32     m_dwVersion = 0x01;
+    guint32     m_dwGrpNum = BLKGRP_NUMBER;
+    gint32      m_iFd;
+
+    gint32 FreeBlocks(
+        const guint32* pvecBlocks,
+        guint32 dwNumBlocks );
+
+    gint32 AllocBlocks( guint32 dwNumBlocks,
+        std::vector< guint32 >& vecBlocks );
+
+    // write pBuf to qwLogicAddr, till the end of the
+    // block or the end of the pBuf
+    gint32 WriteBlock(
+        guint64 qwLogicAddr, BufPtr pBuf );
+
+    // write pBuf to an array of blocks, till all the
+    // blocks are written or all the bytes are written
+    // from the pBuf.
+    gint32 WriteBlocks( const guint64* pBlocks,
+        guint32 dwNumBlocks, BufPtr pBuf );
+
+    // read block at qwLogicAddr to pBuf, till the end of the
+    // block.
+    gint32 ReadBlock(
+        guint64 qwLogicAddr, BufPtr pBuf );
+
+    // read blocks to pBuf specified by logic addresses
+    // from pBlocks, till the end of the pBlocks.
+    gint32 ReadBlocks( const guint64* pBlocks,
+        guint32 dwNumBlocks, BufPtr& pBuf );
 };
