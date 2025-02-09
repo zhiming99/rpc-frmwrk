aafca8e3a04e99a12f11673a74593652658e327c
zhiming <woodhead99@gmail.com>
2025-02-09 23:13:30 +0800

    . monconst.h: added a missing header file

---
+++ monitor/appmon/monconst.h
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  monconst.h
+ *
+ *    Description:  declarations of constants of the monitor module  
+ *
+ *        Version:  1.0
+ *        Created:  02/07/2025 02:17:38 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ming Zhi(woodhead99@gmail.com )
+ *   Organization:
+ *
+ *      Copyright:  2025 Ming Zhi( woodhead99@gmail.com )
+ *
+ *        License:  This program is free software; you can redistribute it
+ *                  and/or modify it under the terms of the GNU General Public
+ *                  License version 3.0 as published by the Free Software
+ *                  Foundation at 'http://www.gnu.org/licenses/gpl-3.0.html'
+ *
+ * =====================================================================================
+ */
+#pragma once
+#define APPS_ROOT_DIR       "apps"
+#define MONITOR_STREAM_DIR  "notify_streams"
+#define OWNER_STREAM_FILE   "owner_stream"
+#define POINT_TYPE_FILE     "ptype"
+#define OUTPUT_LINKS_DIR    "ptrs"
