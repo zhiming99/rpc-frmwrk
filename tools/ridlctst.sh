2af0c723fe50653ac49535137e17dcbea4cbc30f
zhiming <woodhead99@gmail.com>
2025-02-20 20:18:52 +0800

    . ridlctst.sh: fixed some bugs.

--- tools/ridlctst.sh
+++ tools/ridlctst.sh
@@ -1,125 +1,135 @@
 #!/bin/sh
-a=(./ridl/fulltest.ridl ./monitor/appmon/appmon.ridl)
+lib_dir=/usr/local/lib
+export PATH=/usr/lib/ccache:/usr/local/bin:/usr/local/bin/rpcf:$PATH
+export LD_LIBRARY_PATH=${lib_dir}:${lib_dir}/rpcf
+mkdir testridlc
+pushd testridlc
+a=(../ridl/fulltest.ridl ../monitor/appmon/appmon.ridl)
+
 for i in ${a[@]}; do
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc -lsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -bsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -lsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -sf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -s -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -f -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -lsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -sf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -s -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -f -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc -bsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -bsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -bf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -bs -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --client -b -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -bsf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -bf -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -bs -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 
-rm *.cpp *.h *.new
+rm -rf *.cpp *.h *.new
 ridlc --server -b -O . $i
-if ! make debug -j8; then
-    exit $?
+if ! make debug -j4; then
+    exit 1
 fi
 done
 
+rm -rf *.cpp *.h *.new
 ridlc --server --sync_mode IUserManager=async --sync_mode \
 IAppStore.ListApps=sync --services AppManager,SimpleAuth -l -O . \
-monitor/appmon/appmon.ridl || exit $?
+${a[1]} || exit 1
 
-ridlc --client --sync_mode IUserManager=sync --sync_mode \
-IAppStore=sync --services AppManager,SimpleAuth -bsf -O . \
-monitor/appmon/appmon.ridl || exit $?
+rm -rf *.cpp *.h *.new
+ridlc --client --sync_mode IUserManager=async --sync_mode \
+IAppStore=async_p --services AppManager,SimpleAuth -l -O . \
+${a[1]} || exit 1
+
+popd
