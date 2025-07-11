e42b97523e4ce1fc9bbd060fcb5f953955c0128c
zhiming <woodhead99@gmail.com>
2025-07-11 22:21:22 +0800

    . updatekeys.py, Makefile.am: added updatekeys.py to the installation list. . rpcfctl: integrated more commands about key management and user management. . inituser.sh: added user 'admin' to group 'admin' by default.

--- monitor/appmon/updatekeys.py
+++ monitor/appmon/updatekeys.py
@@ -4,8 +4,6 @@ import sys
 import json
 from pathlib import Path
 
-home_dir = str(Path.home()) + "/.rpcf"
-
 def update_keys(json_path, ssl_type, priv_key, pub_cert, cacert):
     try:
         with open(json_path, 'r', encoding='utf-8') as f:
@@ -33,22 +31,18 @@ def update_keys(json_path, ssl_type, priv_key, pub_cert, cacert):
 
         # Find the matching port entry
         found = False
-        if ssl_type.lower() == "openssl":
-            home_dir = home_dir + "/openssl"
-        elif ssl_type.lower() == "gmssl":
-            home_dir = home_dir + "/gmssl"
-
-        keyfile_path = [ home_dir + "/" + priv_key, home_dir + "/" + pub_cert  ]
+        keyfile_path = [ priv_key, pub_cert  ]
         if cacert is not None:
-             keyfile_path.append( home_dir + "/" + cacert )
+             keyfile_path.append( cacert )
 
         for port in data.get("Ports", []):
             if port.get("PortClass") == port_class:
                 params = port.get("Parameters", {})
                 params["CertFile"] = keyfile_path[1]
                 params["KeyFile"] = keyfile_path[0]
-                if os.path.getsize( keyfile_path[2] ) > 0:
-                    params["CACertFile"] = keyfile_path[2]
+                if len( keyfile_path ) >= 3 :
+                    if os.path.getsize( keyfile_path[2] ) > 0:
+                        params["CACertFile"] = keyfile_path[2]
                 else:
                     params["CACertFile"] = ""
                 port["Parameters"] = params
@@ -61,34 +55,36 @@ def update_keys(json_path, ssl_type, priv_key, pub_cert, cacert):
         if not ssl_enabled:
             return 0
 
-        for match in data.get("Matches", []):
+        for match in data.get("Match", []):
             if match.get("PortClass") == "TcpStreamPdo2":
                 ps = match.get("ProbeSequence", [])
                 if ssl_type.lower() == "openssl":
                     ssldrv ="RpcOpenSSLFidoDrv"
                 else:
                     ssldrv ="RpcGmSSLFidoDrv"
-                if ( ps[ -1 ] == "RpcOpenSSLFidoDrv" or
-                    ps[ -1 ] == "RpcGmSSLFidoDrv" ):
-                    ps[ -1 ] = ssldrv
+                if ( ps[ 0 ] == "RpcOpenSSLFidoDrv" or
+                    ps[ 0 ] == "RpcGmSSLFidoDrv" ):
+                    ps[ 0 ] = ssldrv
                 else:
-                    ps.append( ssldrv )
+                    ps.insert( 0, ssldrv )
                 match[ "ProbeSequence" ] = ps
                 if ssl_type.lower() == "openssl":
                     match[ "UsingGmSSL" ] = "false"
                 else:
                     match[ "UsingGmSSL" ] = "true"
-
         with open(json_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
-        print(f"Updated keys successfully")
+            print(f"Updated keys successfully")
     except Exception as err:
         print( err )
         sys.exit(1)
     return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python update_keys.py <path to driver.json> <openssl|gmssl> <server|client>")
+    if len(sys.argv) < 5:
+        print("Usage: python updatekeys.py <path to driver.json> <openssl|gmssl> <priv_key> <pub_cert> [<ca cert>]")
         sys.exit(1)
-    update_keys(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) == 5:
+        update_keys(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[ 4 ], None)
+    else:
+        update_keys(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[ 4 ], sys.argv[ 5 ])
