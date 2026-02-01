0b1abca1abb6ae4e248fd82a89f93c43eeed8771
zhiming <woodhead99@gmail.com>
2026-02-01 13:46:49 +0800

    . c-cpp.yml, regtest.sh: added regtest.sh to the workflow as registry test.

---
+++ tools/regtest.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+if ! wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz; then
+    exit 1
+fi
+
+mkdir ./regtest
+mntdir=./regtest
+/usr/local/bin/regfsmnt -i registry.dat
+/usr/local/bin/regfsmnt -sd registry.dat $mntdir
+cnt=0
+while not mountpoint -q $mntdir; do
+    sleep 1
+    ((cnt++))
+    if [ $cnt -gt 10 ]; then
+        echo "Failed to mount registry.dat at $mntdir"
+        exit 1
+    fi
+done
+echo creating files and directories test...
+tar Jxf linux-6.12.10.tar.xz -C $mntdir
+umount $mntdir
+echo checking registry
+/usr/local/bin/regfschk registry.dat > regout
+if grep -i Error regout; then
+    exit 1
+fi
+
+echo deleting files and directories test...
+/usr/local/bin/regfsmnt -sd registry.dat $mntdir
+cnt=0
+while not mountpoint -q $mntdir; do
+    sleep 1
+    ((cnt++))
+    if [ $cnt -gt 10 ]; then
+        echo "Failed to mount registry.dat at $mntdir"
+        exit 1
+    fi
+done
+tar Jxf linux-6.12.10.tar.xz -C $mntdir
+ls -l $mntdir/
+umount $mntdir
+echo checking registry again...
+/usr/local/bin/regfschk registry.dat > regout
+rm registry.dat linux-6.12.10.tar.xz
+if grep -i Error regout; then
+    exit 1
+fi
+exit 0
