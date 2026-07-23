9a45221240ecbbdf5858d887c50369a9d0b93faf
zhiming <woodhead99@gmail.com>
2026-07-23 16:07:16 +0800

    . buildall-deb.sh.in, buildall-rasp.sh.in: fixed regression bugs introduced when replacing the installation of nvm with the direct installation of nodejs. . c-cpp-2-cmake.yml, CMakeLists.txt, postinst.cmake.in, postrm.cmake.in: replaced dependency of python3-pip with python3-installer to avoid the big footprint caused by installing python3-pip.

--- tools/buildall-rasp.sh
+++ tools/buildall-rasp.sh
@@ -9,10 +9,10 @@ fi
 echo installing development tools...
 ${SUDO} apt-get -y install tzdata
 ${SUDO} apt-get install -y gcc g++ python3 python3-dev python3-pip flex bison \
-libtool shtool automake autoconf autotools-dev make dbus dbus-bin libdbus-1-3 \
-libdbus-1-dev libjsoncpp-dev libkrb5-3 libkrb5-dev liblz4-1 \
-liblz4-dev openssl libssl1.1 libssl-dev libcppunit-1.14-0 libcppunit-dev \
-libfuse3-3 libfuse3-dev bash net-tools procps swig default-jdk-headless cmake \
+libtool shtool automake autoconf autotools-dev make dbus dbus-bin \
+libdbus-1-dev libjsoncpp-dev libkrb5-dev python3-build \
+liblz4-dev openssl libssl-dev libcppunit-dev \
+libfuse3-dev bash net-tools procps swig default-jdk-headless cmake \
 libcommons-cli-java ccache curl fuse3 python3-urwid gettext
 
 ${SUDO} apt-get -y install sip-tools || ${SUDO} apt-get -y install sip-dev python3-sip python3-sip-dev || true
@@ -20,9 +20,9 @@ ${SUDO} apt-get -y install sip-tools || ${SUDO} apt-get -y install sip-dev pytho
 ${SUDO} apt-get -y install git devscripts debhelper expect screen vim
 ${SUDO} apt-get -y install python3-wheel python3-numpy || pip3 install wheel numpy
 
-ARG NODE_MAJOR=22
-RUN curl -fsSL https://deb.nodesource.com/setup_${NODE_MAJOR}.x | bash - || exit 3
-RUN apt-get install -y nodejs
+NODE_MAJOR=22
+curl -fsSL https://deb.nodesource.com/setup_${NODE_MAJOR}.x | bash - || exit 3
+apt-get install -y nodejs
 
 npm -g install assert browserify buffer exports long lz4 process put safe-buffer stream xxhashjs xxhash webpack webpack-cli minify vm events crypto-browserify stream-browserify
 
