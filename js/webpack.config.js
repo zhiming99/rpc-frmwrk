50acb767d83afd3e6004236689ae5ac6b0ba5145
zhiming <woodhead99@gmail.com>
2026-07-24 18:19:03 +0800

    . webpack.config.js: continued fixing the build failure.

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -23,20 +23,8 @@ module.exports = {
         }),
     ],
     resolve: {
-        // Tells Webpack: "Stop evaluating the absolute global path.
-        // Treat this folder as a local directory right here in the project."
-        symlinks: false,
-        // Explicitly verify Webpack checks your linked folder name
-        modules: ['node_modules']
-    },
-    module: {
-        rules: [
-            {
-            test: /\.js$/,
-            // Because symlinks is false, Webpack shields everything inside
-            // the local node_modules folder—preventing the stray upstream crash.
-            exclude: /node_modules/
-            }
-        ]
-  }
+        alias: {
+            '@ljharb/tsconfig': false
+        }
+    }
 }
