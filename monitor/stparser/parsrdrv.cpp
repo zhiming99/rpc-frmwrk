de8de2b642a3427393cc52171843aa5cb3aa8dc5
zhiming <woodhead99@gmail.com>
2026-09-10 23:07:29 +0800

    . parsrdrv.cpp: added a small change

--- monitor/stparser/parsrdrv.cpp
+++ monitor/stparser/parsrdrv.cpp
@@ -406,9 +406,12 @@ gint32 AdjustTokens(
                 if( current_tok == TOK_DOT &&
                     next_tok == TOK_ID )
                 {
-                    // assuming instance_path not span lines.
+                    // assuming instance_path not span lines
+                    // and have not spaces in the middle.
                     if( current_lloc.first_line ==
-                        prev_lloc.first_line )
+                        prev_lloc.first_line &&
+                        current_lloc.first_column ==
+                            prev_lloc.last_column + 1 )
                         bInsert = false;
                 }
                 if( !bInsert )
